2a73d2a9e9c78aa5bddc8c5796225c5cfeba5624
Lennart Hess <lennart.hess@monday-consulting.com>
2021-04-16 11:22:27 +0200

    initial commit

---
+++ src/main/java/com/gruppe4/boredoom/backend/spring/BoredoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gruppe4.boredoom.backend.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredoomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoredoomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gruppe4/boredoom/backend/spring/BoredoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppe4.boredoom.backend.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredoomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
