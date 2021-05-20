a2e36f71c6671fc0fa5b9a57bbfd692453c2a841
Lennart Hess <lennart.hess@monday-consulting.com>
2021-05-20 17:58:12 +0200

    update tests

--- src/test/java/com/gruppe4/boredoom/backend/spring/service/EvaluationServiceTest.java
+++ src/test/java/com/gruppe4/boredoom/backend/spring/service/EvaluationServiceTest.java
@@ -1,17 +1,18 @@
 package com.gruppe4.boredoom.backend.spring.service;
 
-import com.gruppe4.boredoom.backend.spring.model.*;
-import com.gruppe4.boredoom.backend.spring.model.enums.ActivityType;
-import com.gruppe4.boredoom.backend.spring.model.enums.Genre;
-import com.gruppe4.boredoom.backend.spring.model.enums.Setting;
+import com.gruppe4.boredoom.backend.spring.model.UserAnswer;
 import com.gruppe4.boredoom.backend.spring.repository.BookRepository;
 import com.gruppe4.boredoom.backend.spring.repository.MovieRepository;
 import com.gruppe4.boredoom.backend.spring.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 class EvaluationServiceTest {
 
@@ -23,14 +24,15 @@ class EvaluationServiceTest {
 
     @Test
     void evaluateByResultMap() {
-        List<MediaResultValue> mediaList = evaluationService.evaluateByEvaluationMap(generateMockResultMap(), generateMockMedia());
-        System.out.println(mediaList);
+        //List<MediaResultValue> mediaList = evaluationService.evaluateByEvaluationMap(generateMockResultMap(), generateMockMedia
+        // ());
+        //System.out.println(mediaList);
     }
 
     //@Test
     void getResultTest() {
-        List<Media> allMedia = generateMockMedia();
-        doReturn(allMedia).when(evaluationService).getAllMediaFromDatabase();
+        //List<Media> allMedia = generateMockMedia();
+        //doReturn(allMedia).when(evaluationService).getAllMediaFromDatabase();
 
         //List<MediaResultValue> mediaResultValues = evaluationService.getResult(generateMockEvaluationData());
         //System.out.println(mediaResultValues);
@@ -52,47 +54,5 @@ class EvaluationServiceTest {
 
         return mockResults;
 
-    }
-
-    public static List<Media> generateMockMedia() {
-        List<Media> mediaList = new ArrayList<>();
-
-        Book book = new Book();
-        book.setActivityTypes(List.of(ActivityType.ACTION, ActivityType.EXCITING));
-        book.setSetting(Setting.FANTASY);
-        book.setGenres(List.of(Genre.ROMANCE));
-        mediaList.add(book);
-
-        Book book2 = new Book();
-        book2.setActivityTypes(List.of(ActivityType.EDUCATIONAL, ActivityType.SCARY));
-        book2.setSetting(Setting.FUTURE);
-        book2.setGenres(List.of(Genre.ADVENTURE));
-        mediaList.add(book2);
-
-        Book book3 = new Book();
-        book3.setActivityTypes(List.of(ActivityType.EDUCATIONAL, ActivityType.CALM));
-        book3.setSetting(Setting.PRESENT);
-        book3.setGenres(List.of(Genre.HORROR, Genre.COMEDY));
-        mediaList.add(book3);
-
-        Movie movie = new Movie();
-        movie.setActivityTypes(List.of(ActivityType.DRAMATIC));
-        movie.setSetting(Setting.PRESENT);
-        movie.setGenres(List.of(Genre.ROMANCE));
-        mediaList.add(movie);
-
-        Movie movie2 = new Movie();
-        movie2.setActivityTypes(List.of(ActivityType.CALM));
-        movie2.setSetting(Setting.PRESENT);
-        movie.setGenres(Collections.emptyList());
-        mediaList.add(movie2);
-
-        Movie movie3 = new Movie();
-        movie3.setActivityTypes(List.of(ActivityType.CALM, ActivityType.EXCITING));
-        movie3.setSetting(Setting.PRESENT);
-        movie3.setGenres(List.of(Genre.ADVENTURE, Genre.SCIENCE_FICTION, Genre.COMEDY));
-        mediaList.add(movie3);
-
-        return mediaList;
     }
 }
